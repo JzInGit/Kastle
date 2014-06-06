613989467c5f49652b45d9dbbcd0937e9941a7a4
Owen Klein <jz.ok@hotmail.com>
2014-06-06 17:27:41 +0800

    d

--- src/main/java/com/klein/exception/OKUtilException.java
+++ src/main/java/com/klein/exception/OKUtilException.java
@@ -5,6 +5,11 @@ public class OKUtilException extends Exception
 
     private static final long serialVersionUID = -2284896076969842871L;
 
+    public OKUtilException()
+    {
+        super();
+    }
+    
     public OKUtilException(String errorMsg)
     {
         super(errorMsg);
