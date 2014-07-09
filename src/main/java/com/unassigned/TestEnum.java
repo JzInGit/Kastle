f63f95f6c5b9e85e47c9d2ab1350f703d21dfbb0
Owen Klein <jz.ok@hotmail.com>
2014-07-09 15:29:18 +0800

    nul

--- src/main/java/com/unassigned/TestEnum.java
+++ src/main/java/com/unassigned/TestEnum.java
@@ -4,6 +4,15 @@ public class TestEnum
 {
     public static void main(String[] args)
     {
-        System.out.println(Fruit.APPLE.toString());
+        System.out.println(getFruit(null));
+    }
+
+    public static String getFruit(Fruit fruit)
+    {
+        if (fruit == null)
+        {
+            return "Invalid input";
+        }
+        return fruit.toString();
     }
 }
--- src/main/java/com/unassigned/TestReflect.java
+++ src/main/java/com/unassigned/TestReflect.java
@@ -2,10 +2,10 @@ package com.unassigned;
 
 public class TestReflect
 {
+
     public static void main(String[] args)
     {
-        TestEnum enum1 = new TestEnum();
-        System.out.println(enum1.getClass().getName());
+
     }
 
 }
